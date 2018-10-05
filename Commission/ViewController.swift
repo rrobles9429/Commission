56115b269bb1c7df89cdb8709d0c8c3e72854431
Rebecca Robles <rrobles9429@jh-mba158-17.d214.org>
2018-10-05 11:14:00 -0500

    Initial Commit

---
+++ Commission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Commission
+//
+//  Created by Rebecca Robles on 10/5/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
