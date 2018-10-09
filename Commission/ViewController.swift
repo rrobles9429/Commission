d45d78ae7352d8266045fe1ea10bd7bcd53cb995
Rebecca Robles <rrobles9429@jh-mba158-17.d214.org>
2018-10-09 11:06:07 -0500

    Added IB Outlets and IB Action

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var totalPayLabel: UILabel!
+    @IBOutlet weak var commissionPayTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    @IBAction func calculateOnTapped(_ sender: UIButton) {
+    }
+    
+    
 }
 
